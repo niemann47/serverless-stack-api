4aadf1bcc3f7577c662d94fababc9291cb8e36bc
Nick Niemann <niemann47@gmail.com>
2020-04-22 22:01:53 -0700

    add line break in embedding the attachment

--- smtp.js
+++ smtp.js
@@ -7,7 +7,6 @@ export async function main(event, context) {
     TableName: process.env.smtpTableName,
     // 'Key' defines the partition key and sort key of the item to be updated
     // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'noteId': path parameter
     Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
     },
